25299d418a382c3b517a55d64e74f0d54dd33c97
Yohan9H <yhurtebize58@gmail.com>
2025-07-26 23:24:10 +0200

    add ex02

---
+++ python01/ex02/load_image.py
@@ -0,0 +1,30 @@
+import PIL.Image as Image
+import numpy as np
+
+
+def ft_load(path: str) -> np.array:
+    """Loads an image from the given path, converts it to RGB format,
+    prints its shape, and returns its pixel data as a NumPy array.
+
+    Args:
+        path (str): The file path to the image.
+
+    Returns:
+        np.array: A NumPy array representing the image in RGB format.
+
+    Raises:
+        AssertionError: If the path argument is not a string.
+        Exception: If the image cannot be opened or converted.
+    """
+    try:
+        if not isinstance(path, str):
+            raise AssertionError
+        img = Image.open(path)
+        img = img.convert("RGB")
+        arr = np.array(img)
+        print("The shape of image is:", arr.shape)
+        return arr
+    except AssertionError:
+        print("Error -> arg is bad type")
+    except Exception as err:
+        print("Error ->", err)
