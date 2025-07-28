d73f8a3ac2c3c1f7f9fa25edc3934a97abedcdf6
Yohan9H <yhurtebize58@gmail.com>
2025-07-28 13:16:57 +0200

    add ex04

--- python01/ex04/load_image.py
+++ python01/ex04/load_image.py
@@ -1,29 +1,20 @@
-import numpy as np
 import PIL.Image as Image
-import matplotlib.pyplot as plt
-import load_image as ld
+import numpy as np
 
 
-def ft_zoom():
+def ft_zoom(path: str):
     """ Load and convert 'animal.jpeg' to grayscale, crop a zoomed region,
     print its shape and data, then display it.
 
     Prints errors if loading or processing fails.
     """
     try:
-        ld.ft_load("animal.jpeg")
-        img = Image.open("animal.jpeg").convert("L")
+        img = Image.open(path).convert("L")
         arr = np.array(img)
         zoomed = arr[100:500, 400:800]
         zoomed_1 = np.expand_dims(zoomed, axis=-1)
-        print("New shape after slicing:", zoomed_1.shape, "or", zoomed.shape)
+        print("The shape of image is:", zoomed_1.shape, "or", zoomed.shape)
         print(zoomed_1)
-        plt.imshow(zoomed, cmap="gray")
-        plt.show()
 
     except Exception as err:
         print("Error ->", err)
-
-
-if __name__ == "__main__":
-    ft_zoom()
---
+++ python01/ex04/rotate.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+import PIL.Image as Image
+import numpy as np
+import load_image as ld
+
+
+def ft_rotate():
+    try:
+        ld.ft_zoom("animal.jpeg")
+        img = Image.open("animal.jpeg").convert("L")
+        arr = np.array(img)
+        zoomed = arr[100:500, 400:800]
+        rot = np.zeros((400, 400))
+        for i in range(len(zoomed)):
+            for j in range(len(zoomed[i])):
+                rot[j][i] = zoomed[i][j]
+        print("New shape after Transpose:", rot.shape)
+        print(rot)
+        plt.imshow(rot, cmap="gray")
+        plt.show()
+    except Exception as err:
+        print("Error ->", err)
+
+
+if __name__ == "__main__":
+    ft_rotate()
