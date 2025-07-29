df708c90aa6c8764aca9a73c3a6797994c1b7ce7
Yohan9H <yhurtebize58@gmail.com>
2025-07-29 09:26:31 +0200

    maj

---
+++ python01/ex05/pimp_image.py
@@ -0,0 +1,27 @@
+# import matplotlib.pyplot as plt
+# import PIL.Image as Image
+import numpy as np
+# import load_image as ld
+
+
+def ft_invert(array) -> np.array:
+    try:
+        return array
+    except Exception as err:
+        print("Error ->", err)
+
+
+def ft_red(array) -> np.array:
+    return array
+
+
+def ft_green(array) -> np.array:
+    return array
+
+
+def ft_blue(array) -> np.array:
+    return array
+
+
+def ft_grey(array) -> np.array:
+    return array
