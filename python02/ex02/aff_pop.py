866b91d3920ddff9555e8dac663a5030b74c61a0
Yohan9H <yhurtebize58@gmail.com>
2025-08-06 15:05:53 +0200

    add ex02

---
+++ python02/ex02/aff_pop.py
@@ -0,0 +1,45 @@
+import matplotlib.pyplot as plt
+import matplotlib.ticker as pltk
+import load_csv as load
+
+
+def parse_pop(arg):
+    return arg.str.replace('M', '').astype(float)
+
+
+def aff_pop() -> None:
+    try:
+        df = load.load("population_total.csv")
+        france_df = df[df["country"] == "France"]
+        belgium_df = df[df["country"] == "Belgium"]
+
+        france_values = parse_pop(france_df.drop(columns="country").iloc[0])
+        belgium_values = parse_pop(belgium_df.drop(columns="country").iloc[0])
+
+        years = df.columns[1:].astype(int)
+        mask = (years >= 1800) & (years <= 2050)
+        years = years[mask]
+        france_values = france_values[mask]
+        belgium_values = belgium_values[mask]
+
+        plt.plot(years, belgium_values, label="Belgium", color="blue")
+        plt.plot(years, france_values, label="France", color="green")
+        plt.title("Population Projections")
+        plt.xlabel("Year")
+        plt.ylabel("Population")
+
+        ax = plt.gca()
+        formatter = pltk.FuncFormatter(lambda x, _: f'{int(x)}M')
+        ax.yaxis.set_major_formatter(formatter)
+        ax.set_yticks([20, 40, 60])
+
+        plt.xticks(range(1800, 2041, 40))
+
+        plt.legend(loc="lower right")
+        plt.show()
+    except Exception as err:
+        print("Error ->", err)
+
+
+if __name__ == "__main__":
+    aff_pop()
